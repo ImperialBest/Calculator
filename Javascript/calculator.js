b879ff21019502915e9ab597b278a08a8fcbab3d
Luqmon Fathiat <luqmonfathiatmorenikeji@gmail.com>
2023-11-15 21:31:57 +0100

    my calculator

---
+++ Javascript/calculator.js
@@ -0,0 +1,47 @@
+// alert("Hello")
+
+function SetTime(){
+    currentDate = new Date()
+    currentHour = currentDate.getHours();
+    currentMinute = currentDate.getMinutes();
+    currentSecond = currentDate.getSeconds();
+    time = document.getElementsByClassName("time");
+    hour = time[0];
+    minute = time[2];
+    seconds = time[4];
+    time[1].style.visibility = (time[1].style.visibility == "hidden" )? "visible":"hidden";
+    time[3].style.visibility = time[1].style.visibility;
+    hour.innerHTML = currentHour < 10? '0'+currentHour : currentHour
+    minute.innerHTML = currentMinute < 10? '0'+currentMinute : currentMinute
+    seconds.innerHTML = currentSecond < 10? '0'+currentSecond : currentSecond
+}
+setInterval(SetTime, 500)
+
+userInput = document.querySelector("input")
+console.log(userInput.value)
+
+function ShowValue(num){
+    userInput.value+=num
+}
+function ShowAnswer(){
+    userInput.value += '=' + eval(userInput.value)
+}
+
+
+
+
+
+
+button = -1
+function Increm(){
+    button1 = document.getElementById("but")
+    button += 1
+    button1.innerHTML = button
+}
+// Increm()
+buttonIncrease = 0
+function Increase (){
+    buttonIncrease +=1
+    get = document.getElementById("target")
+    get.innerHTML = buttonIncrease
+}
---
+++ Javascript/java.js
@@ -0,0 +1,73 @@
+realValue = ""
+show = document.getElementsByClassName("screen");
+function New(but, button = but) {
+  if(clear == true){
+    realValue = ""
+    show[0].value =""
+  }
+  show[0].value += but;
+  realValue += button
+  clear = false
+}
+clear = false
+answer =""
+function Math1(){
+  clear = true
+  answer =  eval(realValue)
+  adder = show[0].value += ' = ' + answer
+}
+function Answer(){
+  New('Ans',answer)  
+}
+function DelKey(){
+  lengthOfInput = show[0].value.length
+  show[0].value = show[0].value.substring(0, lengthOfInput-1)
+  realValue = show[0].value.substring(0, lengthOfInput-1)
+  // console.log(lengthOfInput)
+}
+function ClearAll(){
+  show[0].value = ""
+  realValue = ""
+  clear = true
+}
+theVisibility = "hidden"
+function Time(){
+  instanceDate = new Date();
+  hours = instanceDate.getHours();
+  minutes = instanceDate.getMinutes();
+  seconds = instanceDate.getSeconds();
+  timeChange = document.getElementsByClassName("time");
+  timeChange[0].innerHTML = hours;
+  timeChange[1].innerHTML = ':';
+  timeChange[2].innerHTML = minutes;
+  timeChange[3].innerHTML = ':';
+  timeChange[4].innerHTML = seconds;
+  if (theVisibility == "hidden"){
+    theVisibility = "visible"
+  }
+  else {
+    theVisibility = "hidden"
+  }
+  timeChange[1].style.visibility = theVisibility
+  timeChange[3].style.visibility = theVisibility
+}
+// Time();
+setInterval(Time,500)
+
+
+
+
+
+
+
+
+
+
+// function Exponential(){
+//   expo = '^' + show[0].value + ' = ' + show[0].value ** show[0].value
+//   show[0].value += expo
+// }
+// function sqr (){
+//   expo = + = ' + Math.pow(show[0].value,show[0].value)
+//   show[0].value += expo
+// }
